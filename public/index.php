7dc34962bdd038a3e01cda45c4df342c797d286b
v-dem <vitaly_demyanenko@yahoo.com>
2021-06-07 16:49:19 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ define('QUEASY_START_TIME', isset($_SERVER['REQUEST_TIME_FLOAT'])
     ? $_SERVER['REQUEST_TIME_FLOAT']
     : microtime(true));
 
-define('QUEASY_PUBLIC_PATH', __DIR__ . (DIRECTORY_SEPARATOR === __DIR__[strlen(__DIR__) - 1])? '': DIRECTORY_SEPARATOR);
+define('QUEASY_PUBLIC_PATH', __DIR__ . (DIRECTORY_SEPARATOR === __DIR__{strlen(__DIR__) - 1})? '': DIRECTORY_SEPARATOR);
 define('QUEASY_ROOT_PATH', QUEASY_PUBLIC_PATH . '..' . DIRECTORY_SEPARATOR);
 define('QUEASY_CONFIG_PATH', QUEASY_ROOT_PATH . 'config.php');
 
