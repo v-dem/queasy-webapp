6dd973c213f8e22ed973a4f7a9f90e937031848c
v-dem <vitaly_demyanenko@yahoo.com>
2021-09-13 17:36:54 +0300

    fix

--- config/router.php
+++ config/router.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    '~/index.php/sign-in~' => 'app\controller\SignIn',
+    '~/index.php/sign-in/?(.*)?~' => 'app\controller\SignIn',
     '~/index.php/sign-up~' => 'app\controller\SignUp',
     '~/~' => 'app\controller\Home'
 );
