d809cdaa1b214f6fd8999e642d86dd342e59a174
v-dem <vitaly_demyanenko@yahoo.com>
2021-09-16 16:03:23 +0300

    fix

--- config/logger.php
+++ config/logger.php
@@ -3,12 +3,14 @@
 return array(
     array(
         'class' => 'queasy\log\FileSystemLogger',
-        'path' => QUEASY_ROOT_PATH . 'logs' . DIRECTORY_SEPARATOR . 'debug.log',
+        'path' => QUEASY_ROOT_PATH . 'logs' . DIRECTORY_SEPARATOR . 'debug.%s.log',
+        'timeLabel' => 'Y-m-d',
         'minLevel' => Psr\Log\LogLevel::INFO
     ),
     array(
         'class' => 'queasy\log\FileSystemLogger',
-        'path' => QUEASY_ROOT_PATH . 'logs' . DIRECTORY_SEPARATOR . 'debug.full.log',
+        'path' => QUEASY_ROOT_PATH . 'logs' . DIRECTORY_SEPARATOR . 'debug.full.%s.log',
+        'timeLabel' => 'Y-m-d',
         'minLevel' => Psr\Log\LogLevel::DEBUG
     )
 );
--- config/router.php
+++ config/router.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    '~/index.php/sign-in/?(.*)?~' => 'app\controller\SignIn',
-    '~/index.php/sign-up~' => 'app\controller\SignUp',
-    '~/~' => 'app\controller\Home'
+    '~/index.php/sign-in/?(.*)?$~' => 'app\controller\SignIn',
+    '~/index.php/sign-up$~' => 'app\controller\SignUp',
+    '~/?$~' => 'app\controller\Home'
 );
 
