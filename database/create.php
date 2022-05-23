c95ef82a6dfa0c5e578577550b70aa47d57d4e03
v-dem <vitaly_demyanenko@yahoo.com>
2022-05-23 06:31:57 +0300

    autocreate database on project create - update

--- database/create.php
+++ database/create.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../vendor/autoload.php');
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $db = new queasy\db\Db(['connection' => ['path' => __DIR__ . '/database.sqlite']]);
 
